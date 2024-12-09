c3f26be22a0e05ff1a4663d54474e91b45dddf9c
Akshatgarg221 <154924367+Akshatgarg221@users.noreply.github.com>
2024-12-09 12:36:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+const scrollToTopButton = document.querySelector('.scroll-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    scrollToTopButton.style.display = 'block';
+  } else {
+    scrollToTopButton.style.display = 'none';
+  }
+});
+
+scrollToTopButton.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
